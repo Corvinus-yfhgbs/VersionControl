0b068b0c6abf4e5c1adf0ec5fa95d78abd6c8e76
Miriam Matolcsi <miriam@matolcsi.com>
2021-10-06 15:50:16 +0200

    10.06

--- 4.gyakorlat/Form1.Designer.cs
+++ 4.gyakorlat/Form1.Designer.cs
@@ -29,13 +29,19 @@ namespace _4.gyakorlat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(472, 399);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
 
