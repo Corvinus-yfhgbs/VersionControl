11d0f20b63010f8253a79506b1b2b93710679ebe
Miriam Matolcsi <miriam@matolcsi.com>
2021-10-21 12:40:51 +0200

    Portfólió tárolása a memóriában

--- Részvények/Form1.cs
+++ Részvények/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Részvények.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,7 @@ namespace Részvények
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
@@ -23,6 +25,16 @@ namespace Részvények
             context.Tick.Load();
             Ticks = context.Tick.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
